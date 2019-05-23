e8f446bdcc87ef1e0c09aa738bbbbcffa0ac75d5
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-23 22:10:43 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, langs|
+    langs.each do |lang, info|
+      info.each do |type, typev|
+        if new_hash.has_key?(lang)
+          new_hash[lang][:style] << style
+        else
+        new_hash[lang] = {
+          type: typev,
+          style: [style]
+        }
+        end
+
+      end
+    end
+  end
+  new_hash
+
 end
