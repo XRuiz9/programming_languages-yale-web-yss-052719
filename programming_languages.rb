2e5afedd7d9da7f94ec1836408a0afe0d80a4426
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-23 21:53:05 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,9 @@ def reformat_languages(languages)
   languages.each do |style, langs|
     langs.each do |lang, info|
       info.each do |type, typev|
+        if new_hash[lang][:style}.size] == 1
+          new_hash[lang][:style] << style
+        end
         new_hash[lang] = {
           type: typev,
           style: [style]
